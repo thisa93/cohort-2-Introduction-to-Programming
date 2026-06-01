23da3c5b675ff556a7455c0409d0306ee20858d2
thisa93 <thisa93@gmail.com>
2026-06-01 16:50:01 +0530

    feat: Function had been introduced

---
+++ 20260601 01/myFirstJavaScript.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+calculator(25,13);
+
+function calculator(number1, number2){
+    let num1 = number1;
+    let num2 = number2;
+    console.log("Summation of "+num1+" and "+num2+" = "+(num1+num2));
+}
